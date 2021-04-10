44016a97b6a6d137d19c6d98a796b62a4b4a15af
root <root@localhost.localdomain>
2021-04-10 00:00:30 -0700

    update

--- docker/merged_list_file.sh
+++ docker/merged_list_file.sh
@@ -117,4 +117,4 @@
 13 8,16,20 * * * node /scripts/jd_jxd.js |ts >> /scripts/logs/jd_jxd.log 2>&1
 
 # 必须要的默认定时任务请勿删除
-54 14 * * * docker_entrypoint.sh |ts >> /scripts/logs/default_task.log 2>&1
+57 22 * * * docker_entrypoint.sh |ts >> /scripts/logs/default_task.log 2>&1
