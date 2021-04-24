b15014e84dfd520fac5b1f114e04449dca5bab9c
root <root@localhost.localdomain>
2021-04-24 16:33:09 -0700

    update

--- docker/merged_list_file.sh
+++ docker/merged_list_file.sh
@@ -122,4 +122,4 @@
 10 0,23 * * * sleep $((RANDOM % $RANDOM_DELAY_MAX)); node /scripts/jd_tcl.js |ts >> /scripts/logs/jd_tcl.log 2>&1
 
 # 必须要的默认定时任务请勿删除
-32 7 * * * docker_entrypoint.sh |ts >> /scripts/logs/default_task.log 2>&1
+16 16 * * * docker_entrypoint.sh |ts >> /scripts/logs/default_task.log 2>&1
