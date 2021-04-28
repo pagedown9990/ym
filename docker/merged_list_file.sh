3c7c010926a145d7c06d2b0f57e9a050a9c870ab
root <root@localhost.localdomain>
2021-04-28 00:54:09 -0700

    update

--- docker/merged_list_file.sh
+++ docker/merged_list_file.sh
@@ -124,4 +124,4 @@
 5 0 * * * sleep $((RANDOM % $RANDOM_DELAY_MAX)); node /scripts/jd_jin_tie.js |ts >> /scripts/logs/jd_jin_tie.log 2>&1
 
 # 必须要的默认定时任务请勿删除
-53 15 * * * docker_entrypoint.sh |ts >> /scripts/logs/default_task.log 2>&1
+39 11 * * * docker_entrypoint.sh |ts >> /scripts/logs/default_task.log 2>&1
