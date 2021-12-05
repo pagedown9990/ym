1a504cc76d129a926f78ead8251f98360feea59f
root <root@localhost.localdomain>
2021-12-05 08:00:13 +0000

    update

--- docker/merged_list_file.sh
+++ docker/merged_list_file.sh
@@ -141,4 +141,4 @@
 15 0,12,22 * * * sleep $((RANDOM % $RANDOM_DELAY_MAX)); node /scripts/jd_jxmc.js |ts >> /scripts/logs/jd_jxmc.log 2>&1
 
 # 必须要的默认定时任务请勿删除
-22 10 * * * docker_entrypoint.sh |ts >> /scripts/logs/default_task.log 2>&1
+54 0 * * * docker_entrypoint.sh |ts >> /scripts/logs/default_task.log 2>&1
